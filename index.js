c0dd704b56b573b5038e203b4057963071ad7009
Rakib383 <sarkar12ab@gmail.com>
2025-01-17 12:06:57 +0600

    added api endpoint for package data

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.25zkwku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 
@@ -25,10 +25,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const packageCollection = client.db("BanglaQuest").collection("allPackages")
 
 
+    app.get('/packages',async (req,res) => {
+        const result = await packageCollection.find().toArray()
 
-    
+        res.send(result)
+    })
+
+
+    app.get('/packages/:id',async (req,res) => {
+        const id = req.params
+        const query = {_id:new ObjectId(id)}
+        const result = await packageCollection.findOne(query)
+        res.send(result)
+
+    })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
